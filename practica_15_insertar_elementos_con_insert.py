983aa0439ef95585f45dbb5e326eded6142415a5
alexortega0620 <101947138+alexortega0620@users.noreply.github.com>
2022-03-20 21:19:35 -0600

    Add files via upload

---
+++ practica_15_insertar_elementos_con_insert.py
@@ -0,0 +1,11 @@
+
+#insertar elementos con insert
+
+nombres=["alex","cielo","maria","francisco"]
+
+print("FRANCISCO ALEJANDRO ORTEGA GUZMAN 19310472\n")
+
+print("nombres:\n\t0-alex\n\t1-cielo\n\t2-maria\n\t3-francisco\n")
+print("las listas comienzan desde 0,1,2,3, en este caso el nombre que se agregar un nombre enla posicion 0, entonces nuestra lista quedaria:")
+nombres.insert(0,"matias")
+print(nombres)
---
+++ practica_16_ordenar_la_lista_sort_y_revers.py
@@ -0,0 +1,11 @@
+
+#ordenar lista con sort 
+
+nombres=["alex","cielo","maria","francisco"]
+
+print("FRANCISCO ALEJANDRO ORTEGA GUZMAN 19310472\n")
+
+print("nombres:\n\t0-alex\n\t1-cielo\n\t2-maria\n\t3-francisco\n")
+print("las listas comienzan desde 0,1,2,3, en este caso el nombre que se quiere remover es alex, entonces nuestra lista quedaria:")
+nombres.sort()
+print(nombres)
